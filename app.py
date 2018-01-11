2cfdf944164d8a06978d57d12b0b24c457869e32
Adrian <therealao@gmail.com>
2018-01-11 19:47:45 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,245 @@
+from flask import Flask, render_template, json, request,redirect,session,jsonify
+from flaskext.mysql import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+
+mysql = MySQL()
+app = Flask(__name__)
+app.secret_key = 'why would I tell you my secret key?'
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'oooo'
+app.config['MYSQL_DATABASE_DB'] = 'shipdata'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+@app.route('/')
+def main():
+    return render_template('signin.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+@app.route('/showAddWreck')
+def showAddWreck():
+    return render_template('addWreck.html')
+
+@app.route('/showSignin')
+def showSignin():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
+
+@app.route('/getWreck')
+def getWreck():
+    try:
+        if session.get('user'):
+            _user = session.get('user')
+
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('sp_GetWreckByUser',(_user,))
+            wrecks = cursor.fetchall()
+
+            wrecks_dict = []
+            for wreck in wrecks:
+                wreck_dict = {
+                        'Id': wreck[0],
+                        'Title': wreck[1],
+                        'Description': wreck[2],
+                        'Date': wreck[4]}
+                wrecks_dict.append(wreck_dict)
+
+            return json.dumps(wrecks_dict)
+        else:
+            return render_template('error.html', error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html', error = str(e))
+
+@app.route('/addWreck', methods=['POST'])
+def addWreck():
+    try:
+        if session.get('user'):
+            _title = request.form['inputTitle']
+            _description = request.form['inputDescription']
+            _user = session.get('user')
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_addWreck',(_title,_description,_user))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'An error occurred!')
+
+        else:
+            return render_template('error.html',error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+        
+
+        
+        # connect to mysql
+
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+
+        
+
+
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+            
+
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
+
+
+@app.route('/signUp',methods=['POST','GET'])
+def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        # validate the received values
+        if _name and _email and _password:
+            
+            # All Good, let's call MySQL
+            
+
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close() 
+        conn.close()
+
+
+@app.route('/getWreckById', methods=['POST'])
+
+def getWreckById():
+    try:
+        if session.get('user'):
+
+            _id = request.form['id']
+            _user = session.get('user')
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_GetWreckById', (_id, _user))
+            result = cursor.fetchall()
+
+            wreck = []
+            wreck.append({'Id': result[0][0], 'Title': result[0][1], 'Description': result[0][2]})
+
+            return json.dumps(wreck)
+        else:
+            return render_template('error.html', error='Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+
+
+@app.route('/updateWreck', methods=['POST'])
+def updateWreck():
+    try:
+        if session.get('user'):
+            _user = session.get('user')
+            _title = request.form['title']
+            _description = request.form['description']
+            _wreck_id = request.form['id']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_updateWreck', (_title, _description, _wreck_id, _user))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'status': 'OK'})
+            else:
+                return json.dumps({'status': 'ERROR'})
+    except Exception as e:
+        return json.dumps({'status': 'Unauthorized access'})
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/deleteWreck', methods=['POST'])
+def deleteWreck():
+    try:
+        if session.get('user'):
+            _id = request.form['id']
+            _user = session.get('user')
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_deleteWreck', (_id, _user))
+            result = cursor.fetchall()
+
+            if len(result) is 0:
+                conn.commit()
+                return json.dumps({'status': 'OK'})
+            else:
+                return json.dumps({'status': 'An Error occured'})
+        else:
+            return render_template('error.html', error='Unauthorized Access')
+    except Exception as e:
+        return json.dumps({'status': str(e)})
+    finally:
+        cursor.close()
+        conn.close()
+if __name__ == "__main__":
+    app.run(port=5002)
---
+++ static/js/dataPlot.js
@@ -0,0 +1,49 @@
+var data = [[5,4], [3,6], [5,5], [6,2], [3,5]]; //TEST DATA
+
+
+function plotData(){
+    var width = $(".map").width();
+    var height = $(".map").height();
+
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d){return d[0]; })])
+        .range([0, width]);
+
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d){ return d[1]; })])
+        .range([0, height]);
+
+    var graph = d3.select(".map")
+        .append("svg:svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "mapSVG");
+
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return "X: " + d[0] + "</br>"
+            + "Y: " + d[1];
+        })
+
+    graph.call(tip);
+
+    graph.selectAll("scatter-dots")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("cx", function(d, i) { return x(d[0]); } )
+            .attr("cy", function(d, i) { return y(d[1]); } )
+            .attr("r", 5)
+            .attr("fill", "red")
+                .on('mouseover', tip.show)
+                .on('mouseout', tip.hide);
+
+}
+
+window.onload = function() {
+    console.log("DOM loaded!");
+    console.log(data);
+    plotData();
+};
